7ef574f375ab470518c4364d061c880a6f878332
Alex Renner <theonlyalexrenner@icloud.com>
2018-07-13 01:24:31 -0500

    fix ide_helper & composer.lock files

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.27 on 2018-07-12 19:24:58.
+ * Generated for Laravel 5.6.27 on 2018-07-13 01:23:09.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
